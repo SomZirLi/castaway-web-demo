3fe71b1602c4c55e26f05fd30c533fe8c28cff49
Petr Prylepski <somgizili@gmail.com>
2025-05-05 12:57:12 +0300

    [FX] Json path fix

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,11 @@
-const STYLES_JSON_URL = '/data/styles.json'
+const STYLES_JSON_URL = 'data/styles.json'
 
 //get style from json
 export const getStyles = async () => {
   try {
     const response = await fetch(STYLES_JSON_URL);
     if (!response.ok) {
-      throw new Error('Field json: $(response.status)')
+      throw new Error(`Field json: ${response.status}`)
     }
 
     const saveStyles = await response.json();
